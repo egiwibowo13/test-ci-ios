8c69cf4aa4d484354024f03f4612d4cef8f97a86
Egi A Wibowo <egiwibowo13@gmail.com>
2022-10-25 17:15:50 +0700

    first

---
+++ test-ci-ios/test_ci_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_ci_iosApp.swift
+//  test-ci-ios
+//
+//  Created by Egi Wibowo on 25/10/22.
+//
+
+import SwiftUI
+
+@main
+struct test_ci_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
